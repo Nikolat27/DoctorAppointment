8d99f2dfcd72dbc270c69017eb65bfe7ed0209cd
Nikolat27 <alizadehsam31@gmail.com>
2024-02-18 14:04:21 +0330

    project started

--- DoctorAppointment_project/settings.py
+++ DoctorAppointment_project/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'DoctorAppointment_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
